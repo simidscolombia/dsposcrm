91f229c7dbb1787d8f5d687500161fd177f64b5c
discoverysystemsgerente-create <discoverysystems.gerente@gmail.com>
2026-02-16 12:29:11 -0500

    Optimize Database connection & add logs

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,7 +7,14 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  max: 10, // Serverless needs fewer connections
+  connectionTimeoutMillis: 5000 // Error if connection takes too long
+});
+
+console.log('DB Config:', {
+  hasUrl: !!process.env.DATABASE_URL,
+  ssl: true
 });
 
 const db = {
